8a4e86dc7655012f181f606f84e1a72aa618c206
Marc-Aurel Wilzin <49531800+mwilzin@users.noreply.github.com>
2026-07-19 01:50:59 +0200

    fix: use valid Astro trailingSlash enum value

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://mwilzin.github.io/personal-site',
+  base: '/personal-site',
+  trailingSlash: 'never',
+});
