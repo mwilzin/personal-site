a64a3855817504c2635908cd279521b82c42e4b3
Marc-Aurel Wilzin <marc-aurel-w@online.de>
2026-07-19 01:49:14 +0200

    Set trailingSlash to false (boolean) in astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -25,7 +25,7 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
 
 export default defineConfig({
   output: 'static',
-  trailingSlash: 'never',
+  trailingSlash: false,
 
   integrations: [
     sitemap(),
